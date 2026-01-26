931763e4d361f5fffa0a5b3373d12e135b512475
243105004-svg <243105004@alumnos.utn.edu.mx>
2026-01-25 22:02:45 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json");
+header("Access-Control-Allow-Origin: *");
 
 echo json_encode([
     "nombre" => "Santiago Avila Sanchez"
